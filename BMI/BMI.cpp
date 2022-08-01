2f3bfab8548b4f28211ac9255eb56294db47d320
Harry <Harry@LAPTOP-OA4N5M5C>
2022-08-01 11:20:28 +0800

    添加项目文件。

---
+++ BMI/BMI.cpp
@@ -0,0 +1,14 @@
+﻿//Height unit: meter weight unit: kilogram
+
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    double h, w;
+    cin >> h >> w;
+    cout << w / (h * h) << endl;
+}
+
+
